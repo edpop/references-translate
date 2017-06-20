fddf13e923c84173c4d53aa81c9c1d8f20136d28
popov <popov@vl.ru>
2017-06-20 16:19:14 +1000

    Удаляем шаблоны типа ' 324 с.', английский аналог - ' 324 p.'

--- replacements.js
+++ replacements.js
@@ -1,19 +1,21 @@
 class Replacement {
-    /**
-     * @param {RegExp} from
-     * @param {String} to
-     */
-    constructor(from, to) {
-        this.from = from;
-        this.to = to;
-    }
+	/**
+	 * @param {RegExp} from
+	 * @param {String} to
+	 */
+	constructor(from, to) {
+		this.from = from;
+		this.to = to;
+	}
 }
 
 module.exports = [
-    new Replacement(/ С\. /, ' pp. '),
-    new Replacement(/№ (\d+)\./, 'no. $1,'),
-    new Replacement(/ Т\. (\d+)/, ' vol. $1'),
-    new Replacement(/ (\d\d\d\d)\.(.)/, ' $1,$2'),
+	new Replacement(/ С\. /, ' pp. '),
+	new Replacement(/№ (\d+)\./, 'no. $1,'),
+	new Replacement(/ Т\. (\d+)/, ' vol. $1'),
+	new Replacement(/ (\d\d\d\d)\.(.)/, ' $1,$2'),
 	new Replacement(/\. (\d\d\d\d)/, ', $1'),
-    new Replacement(/ \/\/ /, ', ')
+	new Replacement(/ \d+ с\./, ''),
+	new Replacement(/ \d+ p\./, ''),
+	new Replacement(/ \/\/ /, ', ')
 ];
